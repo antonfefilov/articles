b6dd8b31a129686f81c05aa6ef6d8c4d5ebe01ea
haul <h4ul@me.com>
2015-02-07 14:27:40 +0400

    user has many articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,5 +4,6 @@ describe Article do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :text }
 
+  it { is_expected.to belong_to :user }
   it { is_expected.to have_many :comments }
 end
